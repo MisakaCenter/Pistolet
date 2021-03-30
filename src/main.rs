9a750b4597c7113fdcdd5685c9e149d08d18486e
MisakaCenter <misaka@sjtu.edu.cn>
2021-03-30 21:10:30 +0800

    Stack: overflow.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ include!("interpreter.rs");
 
 use colored::*;
 use std::fs;
+use std::thread;
 
 fn main() {
     println!("{}", "[Test] Parser Test Begin!".green());
@@ -15,21 +16,24 @@ fn main() {
         }
     }
     println!("{}", "[Test] Interpreter Test Begin!".green());
-    let file_path = "src/test/test_eval.pst";
-    let unparsed_file = fs::read_to_string(file_path).expect("cannot read file");
+    thread::Builder::new().stack_size(1024000 * 1024).spawn(move || {
+        let file_path = "src/test/test_eval.pst";
+        let eval_result;
+        let unparsed_file = fs::read_to_string(file_path).expect("cannot read file");
+        let state = ProgStates::new();
+        eval_result = ast_eval(parse_to_ast(&unparsed_file).unwrap(), state);
 
-    let state = ProgStates::new();
-    let eval_result = ast_eval(parse_to_ast(&unparsed_file).unwrap(), state);
-    match eval_result {
-        Ok(result) => result.print(),
-        Err(err_code) => match err_code {
-            RuntimeErr::ReturnValue(expr_value) => println!(
-                "[Return] Exit with {} : {}",
-                expr_value.get_type(),
-                expr_value.get_value()
-            ),
-            _ => err_code.print(),
-        },
-    }
+        match eval_result {
+            Ok(result) => result.print(),
+            Err(err_code) => match err_code {
+                RuntimeErr::ReturnValue(expr_value) => println!(
+                    "[Return] Exit with {} : {}",
+                    expr_value.get_type(),
+                    expr_value.get_value()
+                ),
+                _ => err_code.print(),
+            },
+        }
+    }).unwrap().join().expect("Couldn't join on the associated thread");
     println!("{}", "[Test] Interpreter Test Passed!".green());
 }
